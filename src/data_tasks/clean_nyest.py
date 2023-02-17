55890223031f91659c749714b98d5cfb8d22364d
Zoltan Varju <zoltan.varju@gmail.com>
2023-02-17 12:31:30 +0100

    add preprocessing scripts

---
+++ src/data_tasks/clean_nyest.py
@@ -0,0 +1,42 @@
+import html
+import re
+
+import pandas as pd
+
+df = pd.read_csv("data/raw/nyest/contents.csv", sep=";")
+df.fillna('', inplace=True)
+
+CLEANR = re.compile('<.*?>')
+CDATA = re.compile('\/\/\s&lt;!\[CDATA\[\n.*\n\/\/\s*\]\]&gt;')
+
+
+def clean_txt(txt):
+    """Postprocess txt, removes unescaped html entities"""
+    txt = txt.replace("&amp;gt;", " ").replace("&amp;nbsp;", " ").replace("&quot;", " ")
+    txt = txt.replace("&#x27", " ").replace("::adbox::7::", "").replace("&amp;lt;", " ")
+    txt = txt.replace("&amp;amp;", " ")
+    return txt
+
+
+def cleanhtml(raw_html):
+    """Clean raw html page"""
+    cleaned_txt = clean_txt(html.escape(re.sub(CLEANR, ' ', raw_html)))
+    return re.sub(CDATA, ' ', cleaned_txt)
+
+
+with open("data/interim/nyest.txt", "w") as outfile:
+    for _, row in df.iterrows():
+        txt = []
+        if row[0]:
+            txt.append(cleanhtml(row[0]))
+        if row[1]:
+            txt.append(cleanhtml(row[1]))
+        if row[2]:
+            txt.append(cleanhtml(row[2]))
+        if row[3]:
+            txt.append(cleanhtml(row[3]))
+        if row[4]:
+            txt.append(cleanhtml(row[4]))
+        outfile.write(" ".join(txt) + "\n")
+
+print("Done")
---
+++ src/data_tasks/preprocess_oscar.py
@@ -0,0 +1,22 @@
+import os
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+data_root = "data/raw/oscar19"
+text_files = [
+    e for e in os.listdir(data_root) if os.path.isfile(os.path.join(data_root, e))
+]
+
+with open("data/interim/oscar.txt", "w") as outfile:
+    for text_file in text_files:
+        with open(os.path.join(data_root, text_file), "r") as infile:
+            for line in infile:
+                sentences = sent_tokenize(line)
+                for sentence in sentences:
+                    if sentence:
+                        words = word_tokenize(sentence)
+                        words = [word for word in words if word.isalnum()]
+                        if words:
+                            s = " ".join(words)
+                            outfile.write(s + "\n")
+print("Done")
